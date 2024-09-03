1b09adeaa407b722ee5588183ab8fea31fffb53d
Trevor <Trevormahinda@gmail.com>
2024-09-03 17:32:24 +0300

    star

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+// star wars api to retrieve characters from api
+
+const request = require('request');
+const movieid = process.argv[2];
+
+const url = `https://swapi-api.alx-tools.com/api/films/${movieid}`;
+
+function sendRequest (characterList, index) {
+  if (characterList.length === index) {
+    return;
+  }
+
+  request(characterList[index], (error, response, body) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(JSON.parse(body).name);
+      sendRequest(characterList, index + 1);
+    }
+  });
+}
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const characterList = JSON.parse(body).characters;
+
+    sendRequest(characterList, 0);
+  }
+});
